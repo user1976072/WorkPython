bafceee30bac68a79e85649c18f4adec645dd8bd
Olga <user1976072@gmail.com>
2023-09-14 23:25:39 +0300

    7 семинар

--- main.py
+++ main.py
@@ -215,14 +215,14 @@ print(k)
 # print(listq)
 
 # рекурсия фибоначи
-def fib(n):
+# def fib(n):
     
-    if n <= 1:
-        return 1
-    return fib(n-1)+fib(n-2)
+#     if n <= 1:
+#         return 1
+#     return fib(n-1)+fib(n-2)
 
 
-print(fib(7))
+# print(fib(7))
 
 # Хакер Василий получил доступ к классному журналу и хочет заменить все свои минимальные оценки на максимальные. Напишите программу, которая заменяет оценки Василия, но наоборот: все максимальные – на минимальные.
 # Input: 5 -> 1 3 3 3 4
@@ -323,20 +323,100 @@ print(fib(7))
 
 
 # Два различных натуральных числа n и m называются дружественными, если сумма делителей числа n (включая 1, но исключая само n) равна числу m и наоборот. Например, 220 и 284 – дружественные числа. По данному числу k выведите все пары дружественных чисел, каждое из которых не превосходит k. Программа получает на вход одно натуральное число k, не превосходящее 105. Программа должна вывести  все пары дружественных чисел, каждое из которых не превосходит k. Пары необходимо выводить по одной в строке, разделяя пробелами. Каждая пара должна быть выведена только один раз (перестановка чисел новую пару не дает).
-def get_sum(n):
-    s = 1
-    for i in range(2,int(n**0.5)+1):
-        if n%i == 0:
-            s+=i
-            s+= n//i
-    return s
+# def get_sum(n):
+#     s = 1
+#     for i in range(2,int(n**0.5)+1):
+#         if n%i == 0:
+#             s+=i
+#             s+= n//i
+#     return s
 
-k = 1000
-result = []
-for i in range(1,k+1):
-    if i not in result:
-        a = get_sum(i)
-        if i != a and a <= k and get_sum(a) == i:
-            result.append(i)
-            result.append(a)
-print(result)
+# k = 1000
+# result = []
+# for i in range(1,k+1):
+#     if i not in result:
+#         a = get_sum(i)
+#         if i != a and a <= k and get_sum(a) == i:
+#             result.append(i)
+#             result.append(a)
+# print(result)
+
+# Единственный способ вашего взаимодействия с этим кодом - посредством задания функции transformation.
+# Однако вы поняли, что для вашей текущей задачи вам не нужно никак преобразовывать список значений, а нужно получить его как есть.
+# Напишите такое лямбда-выражение transformation, чтобы transformed_values получился копией values.
+# Пример ввода и вывода данных представлены на следующем слайде
+
+
+# values = [1, 23, 42, ‘asdfg’]
+# transformed_values = list(map(trasformation, values))
+# if values == transformed_values:
+#     print(‘ok’)
+# else:
+#     print(‘fail’)
+
+# Вывод: 
+# ok
+
+# transformation = lambda x: x
+# values = [1, 23, 42, "asdfg"]
+# transformed_values = list(map(transformation, values))
+# if values == transformed_values:
+#     print("ok")
+# else:
+#     print("fail")
+
+# def transformation(x):
+#     return x
+# values = [1, 23, 42, "asdfg"]
+# transformed_values = list(map(transformation, values))
+# print(transformed_values)
+
+# Задача про орбиты 
+# Планеты вращаются вокруг звезд по эллиптическим орбитам.
+# Назовем самой далекой планетой ту, орбита которой имеет
+# самую большую площадь. Напишите функцию
+# find_farthest_orbit(list_of_orbits), которая среди списка орбит
+# планет найдет ту, по которой вращается самая далекая
+# планета. Круговые орбиты не учитывайте: вы знаете, что у
+# вашей звезды таких планет нет, зато искусственные спутники
+# были были запущены на круговые орбиты. Результатом
+# функции должен быть кортеж, содержащий длины полуосей
+# эллипса орбиты самой далекой планеты. Каждая орбита
+# представляет из себя кортеж из пары чисел - полуосей ее
+# эллипса. Площадь эллипса вычисляется по формуле S = pi*a*b,
+# где a и b - длины полуосей эллипса. При решении задачи
+# используйте списочные выражения. Подсказка: проще всего
+# будет найти эллипс в два шага: сначала вычислить самую
+# большую площадь эллипса, а затем найти и сам эллипс,
+# имеющий такую площадь. Гарантируется, что самая далекая
+# планета ровно одна
+
+# import math
+
+# def find_farthest_orbit(orbits):
+#     max_area = max([math.pi * a * b for a, b in orbits if a != b])
+#     #
+#     # max_area = 0
+#     # a_max, b_max = 0, 0
+#     # for a, b in orbits:
+#     #     area = math.pi * a * b
+#     #     if area > max_area and a != b:
+#     #         max_area = area
+#     #         a_max, b_max = a, b
+#     #return a_max, b_max
+#     return [(a, b) for a, b in orbits if math.pi * a * b == max_area]
+
+# #orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+
+def same_by(characteristic, objects):
+    # if not objects:  # if objects == []
+    #     return True
+    # s0 = characteristic(objects[0])
+    # for object in objects[1:]:
+    #     s = characteristic(object)
+    #     if s != s0:
+    #         return False
+    #all([True, True, True]) = True
+    #all(True, False, True) = False
+    return all([s == characteristic(objects[0]) if objects else True for s in map(characteristic, objects)])
+    #return True
